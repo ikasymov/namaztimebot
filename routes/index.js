6e358e6a3b90d611aeedfb51f17d46f2723477f1
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-03 17:54:32 +0600

    test req

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const client = require('redis').createClient('redis://h:pc620575a0d2ca6447a07427de2a718cde3f0f974840921dc15ee4e4ae83d1104@ec2-34-231-155-48.compute-1.amazonaws.com:12419');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.body);
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+
+async function requestHandler(req){
+    console.log(req.body)
+}
