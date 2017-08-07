3be4c38b5d2fb1bde2275b8533ca4d8091127e30
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-07 18:07:11 +0600

    test

--- NewChat.js
+++ NewChat.js
@@ -4,10 +4,16 @@ const RequestHandler = require('./handler');
 
 
 function NewChat(req){
-    RequestHandler.apply(this, arguments)
+    RequestHandler.apply(this, arguments);
+    this.messageId = this.data.chat_id
 }
 
 NewChat.prototype = Object.create(RequestHandler.prototype);
 NewChat.prototype.constructor = NewChat;
+
+
+NewChat.prototype.start = async function(){
+    return this._sendMessage('Добро пожаловать введите "старт" что бы узнать время намаза')
+};
 
 module.exports = NewChat;
--- UserFollow.js
+++ UserFollow.js
@@ -1,5 +1,5 @@
 const Follow = require('./Follow');
-
+const request = require('request');
 function UserFollow(req){
     Follow.apply(this, arguments);
 }
@@ -8,14 +8,36 @@ UserFollow.prototype = Object.create(Follow.prototype);
 UserFollow.prototype.constructor = UserFollow;
 
 UserFollow.prototype.signUp = async function(){
+
+};
+UserFollow.prototype.start = async function(){
+    return this.createChat();
+};
+
+
+
+UserFollow.prototype.createChat = async function(){
+    const data = {
+        url: this.apiUrl + '/chats/create',
+        method: 'POST',
+        body: {
+            name: 'new chat',
+            members: [this.user]
+        },
+        headers: {
+            'X-Namba-Auth-Token': this.token
+        },
+        json: true
+    };
     return new Promise((resolve, reject)=>{
-        this.client.set(this.user + 'signUp', true, ((error)=>{
+        request(data, (error, req, body)=>{
             if(!error){
-                resolve(true)
+                console.log(body)
+                resolve(body)
             }
-            reject(false)
-        }))
-    });
+            reject(error)
+        })
+    })
 };
 
 
