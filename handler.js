fe19448dd62546fcce7949bd80fd6116d6a5d114
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-07 11:33:52 +0600

    test

--- handler.js
+++ handler.js
@@ -11,26 +11,27 @@ function RequestHandler(req){
 }
 
 RequestHandler.prototype._sendMessage = async function(message){
-    return true
-    // const data = {
-    //     url: this.apiUrl + '/chats/' + this.messageId + '/write',
-    //     method: 'POST',
-    //     body: {
-    //         type: 'text/plain',
-    //         content: message
-    //     },
-    //     headers: {
-    //         'X-Namba-Auth-Token': this.token
-    //     }
-    // };
-    // return new Promise((resolve, reject)=>{
-    //     request(data, ((error, req, body)=>{
-    //         if(error){
-    //             reject(error)
-    //         }
-    //         resolve(body.success)
-    //     }))
-    // })
+    const data = {
+        url: this.apiUrl + '/chats/' + this.messageId + '/write',
+        method: 'POST',
+        body: {
+            type: 'text/plain',
+            content: message
+        },
+        headers: {
+            'X-Namba-Auth-Token': this.token
+        }
+    };
+    return new Promise((resolve, reject)=>{
+        resolve(true)
+        console.log(data)
+        // request(data, ((error, req, body)=>{
+        //     if(error){
+        //         reject(error)
+        //     }
+        //     resolve(body.success)
+        // }))
+    })
 };
 
 
